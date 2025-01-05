cefdddfdc3499c2527538565ac372db40ae424a4
MostafaHamed-619 <hamedmostafa726@gmail.com>
2025-01-05 12:42:00 +0200

    commit 2

---
+++ ITI.Git.Task1/HelloWorldITI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Git.Task1
+{
+    internal class HelloWorldITI
+    {
+        public void printHelloITI()
+        {
+            Console.WriteLine("Hello from ITI");
+        }
+    }
+}
